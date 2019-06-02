dbd67123aed8880a4b4e694cf885a549a4ed3641
wilsenock220 <wilsenock220@gmail.com>
2019-06-02 21:42:55 +0300

    add heading

--- Wilstan-Quotes/src/app/app.module.ts
+++ Wilstan-Quotes/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteDetailsComponent,
     QuoteComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    SidebarComponent,
+    PostsComponent,
+    UsersComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
