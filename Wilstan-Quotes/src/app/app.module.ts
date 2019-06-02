6220c580300dd022047573ee7f4453da2458c08a
wilsenock220 <wilsenock220@gmail.com>
2019-06-02 21:55:15 +0300

    add styling to the heading

--- Wilstan-Quotes/src/app/app.module.ts
+++ Wilstan-Quotes/src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { PostsComponent } from './posts/posts.component';
-import { UsersComponent } from './users/users.component';
-import { DetailsComponent } from './details/details.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuoteDetailsComponent } from "./quote-details/quote-details.component";
+import { QuoteComponent } from "./quote/quote.component";
+import { QuoteFormComponent } from "./quote-form/quote-form.component";
+
+import { UsersComponent } from "./users/users.component";
+import { DetailsComponent } from "./details/details.component";
 
 @NgModule({
   declarations: [
@@ -18,17 +17,11 @@ import { DetailsComponent } from './details/details.component';
     QuoteDetailsComponent,
     QuoteComponent,
     QuoteFormComponent,
-    SidebarComponent,
-    PostsComponent,
     UsersComponent,
     DetailsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
